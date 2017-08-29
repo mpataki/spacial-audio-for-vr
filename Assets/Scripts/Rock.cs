1fcdc37d5ef80ea88129f657478f5b1f70bf06c4
Mat Pataki <matpataki@gmail.com>
2017-08-29 00:01:11 -0400

    trigger sound on camera collision with rock

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("Initializing Rock");
+		
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("oof, " + other.name + "hit the rock");
+		
+		if (other.name != "First Person Controller")
+			return;
+		
+		if (!audioSource.isPlaying)
+			audioSource.Play();
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Spacial Audio For VR - Test Scene
+# Spacial Audio for VR - Test Scene
 
-This Unity project is a companion to Jen Costa's talk: *Spacial Audio For VR*.
+This Unity project is a companion to Jen Costa's talk: *Spacial Audio for VR*.
 
